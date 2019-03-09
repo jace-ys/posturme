c59669c6db5e01c9914b10f832f52ccc18797233
jace-ys <jaceys.tan@gmail.com>
2019-03-09 21:42:41 +0000

    main page

--- web-client/src/pages/Main.js
+++ web-client/src/pages/Main.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
-import { Container, Progress } from 'semantic-ui-react';
+import { Container, Progress, Card } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
 import socketIOClient from 'socket.io-client';
+
+const MAX_TIME = 20;
 
 export default class Main extends Component {
   state = {
-    time: 360,
-    percent: 0
+    time: MAX_TIME,
+    previous: false,
+    multiplierCount: 0,
+    multiplier: 1,
+    percentage: 0,
+    score: 0,
+    triggerCount: 0
+  }
+
+  endSession = () => {
+
   }
 
   componentDidMount() {
@@ -16,24 +28,89 @@ export default class Main extends Component {
     });
 
     this.socket.on('new-data', message => {
-      console.log(message);
+      const flexValue = JSON.parse(message).flex;
+
+      if (flexValue) {
+        this.setState({
+          previous: true,
+          score: this.state.score + (100 * this.state.multiplier),
+          percentage: this.state.percentage + (100 * 1/MAX_TIME),
+          triggerCount: 0
+        });
+
+        if (this.state.previous) {
+          this.setState({ multiplierCount: this.state.multiplierCount + 1});
+        }
+      }
+
+      else {
+        this.setState({
+          previous: false,
+          multiplierCount: 0,
+          triggerCount: this.state.triggerCount + 1
+        });
+
+        if (this.state.triggerCount % 3 === 0) {
+          speak('Sit straight!');
+        }
+      }
 
       this.setState({
         time: this.state.time - 1,
-        percent: this.state.percent + 5
+        multiplier: computeMultiplier(this.state.multiplierCount),
       });
     });
   }
 
+  componentWillUnmount() {
+    this.socket.close();
+  }
+
   render() {
-    const { time } = this.state;
+    const { time, score, percentage, multiplier } = this.state;
+
+    if (time === 0) {
+      this.socket.close();
+      speak('An hour\'s up! Time to go for a walk!');
+    }
 
     return (
-      <Container style={{ textAlign: 'center', margin: '40px 20px' }}>
-        <h3>Posturmeter</h3>
-        <Progress percent={this.state.percent} indicating/>
-        <h3>Remaining time: {time}</h3>
+      <Container style={{ textAlign: 'center', margin: '20px' }}>
+        <h1>Posturmeter</h1>
+        <h3 style={{ margin: '0' }}>Percentage: {percentage}%</h3>
+        <h5 style={{ margin: '0' }}>Hit 100% for double points!</h5>
+        <Progress percent={percentage} indicating/>
+        <h3 style={{ margin: '10px 0' }}>Remaining time: {time}</h3>
+        <Card style={{ margin: '60px auto' }}>
+          <Card.Content>
+            <Card.Header>
+              <h1>Score</h1>
+            </Card.Header>
+            <Card.Description>
+              <h1 style={{ fontSize: '40px' }}>{score}</h1>
+            </Card.Description>
+          </Card.Content>
+          <Card.Content extra>
+            <h3>Multiplier: {multiplier}x</h3>
+          </Card.Content>
+        </Card>
+        <Link to="/" className="ui button red fluid">End Session</Link>
       </Container>
     );
   }
+}
+
+function computeMultiplier(count) {
+  if (count >= 100) return 1.5;
+  else if (count >= 50) return 1.25;
+  else if (count >= 20) return 1.15;
+  else if (count >= 10) return 1.1;
+  else return 1;
+}
+
+function speak(message) {
+  var msg = new SpeechSynthesisUtterance(message)
+  var voices = window.speechSynthesis.getVoices()
+  msg.voice = voices[0]
+  window.speechSynthesis.speak(msg)
 }
--- web-client/src/pages/Menu.js
+++ web-client/src/pages/Menu.js
@@ -4,18 +4,19 @@ import { Link } from 'react-router-dom';
 export default class Main extends Component {
   render() {
     return (
-      <div style={{ textAlign: 'center', margin: '40px 20px' }}>
+      <div style={{ textAlign: 'center', margin: '20px' }}>
         <img src="/images/landing.svg" alt="Landing" style={{ width: '250px', height: '250px', margin: 'auto' }}></img>
+        <h3>Total Score: 0</h3>
         <Link to="/main" className="ui green button fluid">
           Posture me!
         </Link>
-        <Link to="/main" className="ui violet button fluid">
+        <Link to="/" className="ui violet button fluid">
           Dashboard
         </Link>
-        <Link to="/main" className="ui blue button fluid">
+        <Link to="/" className="ui blue button fluid">
           Rewards
         </Link>
-        <Link to="/main" className="ui yellow button fluid">
+        <Link to="/" className="ui yellow button fluid">
           Calibrate
         </Link>
       </div>
